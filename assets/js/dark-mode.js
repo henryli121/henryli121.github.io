786bd43cd1ba727cc2984fac19466fcceeb9e07c
Hao Li <5250henry@gmail.com>
2024-06-01 18:10:44 +0800

    dark-mode

--- assets/js/dark-mode.js
+++ assets/js/dark-mode.js
@@ -2,20 +2,16 @@ document.addEventListener("DOMContentLoaded", function() {
     // Check for previously saved dark mode preference
     if (localStorage.getItem("darkMode") === "enabled") {
         document.body.classList.add("dark-mode");
-        document.getElementById("dark-mode-icon").src = "{{ '/assets/images/sun-icon.png' | relative_url }}";
+        document.getElementById("dark-mode-toggle").checked = true;
     }
 });
 
 function toggleDarkMode() {
     var element = document.body;
     element.classList.toggle("dark-mode");
-
-    var icon = document.getElementById("dark-mode-icon");
     if (element.classList.contains("dark-mode")) {
         localStorage.setItem("darkMode", "enabled");
-        icon.src = "{{ '/assets/images/sun-icon.png' | relative_url }}";
     } else {
         localStorage.setItem("darkMode", "disabled");
-        icon.src = "{{ '/assets/images/moon-icon.png' | relative_url }}";
     }
 }
